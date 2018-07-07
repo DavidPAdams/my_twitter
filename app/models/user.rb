27b49a34e6f662b463bb26ac6df1eebf7796da2e
David Adams <david61795@gmail.com>
2018-07-06 23:31:07 -0700

    login with username & styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:username]
   validates :username, presence: true, uniqueness: true
+  validates :email, presence: true
   has_many :tweets
 	serialize :following, Array
   mount_uploader :avatar, AvatarUploader
-
-
 end
